fdf9b7fd515e828020673ffff20810d80cfbad60
lijun <wanglijun206@gmail.com>
2021-06-04 06:44:40 +0200

    3. change

--- src/app/share/netease-ui/netease-slider/netease-slider-helper.ts
+++ src/app/share/netease-ui/netease-slider/netease-slider-helper.ts
@@ -2,3 +2,20 @@ export function sliderEvent(e: Event) {
   e.stopPropagation();
   e.preventDefault();
 }
+
+export function getElementOffset(el: HTMLElement): {top: number; left: number;} {
+  if (!el.getClientRects().length) {
+    return {
+      top: 0,
+      left: 0
+    }
+  }
+  //该方法会返回dom元素盒子相关尺寸信息
+  const rect = el.getBoundingClientRect();
+  //取得元素所在的document节点的window对象
+  const win = el.ownerDocument.defaultView ? el.ownerDocument.defaultView : window;
+  return {
+    top: rect.top + win.pageYOffset,
+    left: rect.left + win.pageXOffset
+  }
+}
